e7bb36c1cdc2c2d6f5621f3f367c48a3b8250437
happypeet <hiyai.peet@gmail.com>
2025-05-16 16:10:29 +0800

    Initial Commit

---
+++ text2card/text2cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  text2cardApp.swift
+//  text2card
+//
+//  Created by happy peet on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct text2cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ text2cardTests/text2cardTests.swift
@@ -0,0 +1,16 @@
+//
+//  text2cardTests.swift
+//  text2cardTests
+//
+//  Created by happy peet on 5/16/25.
+//
+
+import Testing
+
+struct text2cardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
